c631927f2ef3d191d7676759624d86fdc8a868c9
ChiragPundir <chiragpundir64@gmail.com>
2024-11-11 13:41:31 +0000

    variables all complete

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+var Name = "chirag"
+const pi = 3.14
+let Email = "chiragpundir@gmail.com"
+let Course = "B.Tech"
+let sem = "5th"
+
+College = "Uttaranchal"
+// Name = "Pundir"
+// Email = "cp@gmail.com"
+
+console.table([Name,pi,Email,Course,sem,College]);
