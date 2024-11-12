d4351c310cd8f357b39f30c51507c0bd846caec5
ChiragPundir <chiragpundir64@gmail.com>
2024-11-12 12:24:25 +0000

    type conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let num = 1;
+let bool = Boolean(num)
+console.log(bool)
+console.log(typeof bool)
+
+let check = true
+let a =Number(check)
+console.log(a)
+
+let b= "343wew"
+let c = Number(b)
+console.log(typeof c)
+console.log(c)
