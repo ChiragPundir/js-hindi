b97fcb8e206c75496802863bc1bcc15777b3bdae
ChiragPundir <chiragpundir64@gmail.com>
2024-11-13 13:41:29 +0000

    string com

---
+++ 01_basics/string.js
@@ -0,0 +1,17 @@
+let str1 = "chirag"
+let str2 = "pundir"
+// console.log(str1+" "+str2)
+
+// console.log(`my name is ${str1} and my surname is ${str2}.`)
+
+let Name = "chiragPundir"
+let Name2 = "chjhsk jjsh"
+
+// console.log(Name.toUpperCase())
+// console.log(Name.length)
+// console.log(Name.charAt(4))
+// console.log(Name.indexOf("i"))
+// console.log(Name.substring(2,6))
+// console.log(Name.slice(0,6))
+// console.log(Name.replace("r","d"))
+console.log(Name2.trim())
