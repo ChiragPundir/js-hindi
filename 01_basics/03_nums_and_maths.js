1b0b0d2a7ceb9f8b698954e74c5404d1bcc91652
ChiragPundir <chiragpundir64@gmail.com>
2024-11-14 13:41:24 +0000

    number and maths are completed

---
+++ 01_basics/03_nums_and_maths.js
@@ -0,0 +1,24 @@
+const num1 = 100
+// console.log(num1)
+
+const num2 = new Number(100)
+// console.log(num2)
+
+const num3 = 123.8966
+// console.log(num3.toPrecision(3))
+
+const num4 = 1000000
+// console.log(num4.toLocaleString('en-IN'))
+
+// +++++++++++++++MATHS+++++++++++++++//
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) )+min)
+
+console.log(Math.min(4,3,5,2))
