04f598049e2312e709f400f74d3bd9c8160d799d
ChiragPundir <chiragpundir64@gmail.com>
2024-11-18 13:20:48 +0000

    array part 2 completed

---
+++ 02_section_of_js/02_array.js
@@ -0,0 +1,20 @@
+const arr1 = [1,2,3,4]
+const arr2 = [5,6,7,8]
+
+// arr1.push(arr2)
+// console.log(arr1)
+
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
+// const arr4 = [...arr1,...arr2]
+// console.log(arr4)
+
+// const arr5 = [1,2,3,[4,5,6],[7,8,[8,10]]]
+// console.log(arr5.flat(Infinity))
+
+console.log(Array.from("CHIRAG"))
+
+let v1 = "chirag"
+let v2 = "pundir"
+console.log(Array.of(v1,v2))
